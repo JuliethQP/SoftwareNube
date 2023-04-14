3300a0eca2fd8f3cb0a6f2ae0de797648d4fe900
devsjuanh <jc.hernandezp1@uniandes.edu.co>
2023-04-13 21:57:07 -0500

    initial flask project

---
+++ conversion_api/app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask_cors import CORS
+from flask_jwt_extended import JWTManager
+from flask_restful import Api
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dbapp.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = 'frase-secreta'
+app.config['PROPAGATE_EXCEPTIONS'] = True
+
+app_context = app.app_context()
+app_context.push()
+
+cors = CORS(app)
+
+api = Api(app)
+
+jwt = JWTManager(app)
