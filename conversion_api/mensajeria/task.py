520db871ed1d1d23f0c287b431484f17016e6c27
SURAMERICANA\juliqupi <jquinchia@sura.com.co>
2023-04-21 14:53:42 -0500

    redis puerto

--- conversion_api/mensajeria/task.py
+++ conversion_api/mensajeria/task.py
@@ -8,7 +8,7 @@ import requests
 
 
 # celery = Celery('tasks', broker="redis://redis:6379/0")
-celery = Celery('tasks', broker="redis://:redisultramegasecurepassword@35.185.127.60:6379")
+celery = Celery('tasks', broker="redis://:redisultramegasecurepassword@35.185.127.60:6380")
 
 
 FILE_PATH = '/conversion_api/files/'
