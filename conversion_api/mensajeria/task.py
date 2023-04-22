72fd7740571b2c59073600a3690c442c6e3f30ee
SURAMERICANA\juliqupi <jquinchia@sura.com.co>
2023-04-22 13:09:33 -0500

    update puerto

--- conversion_api/mensajeria/task.py
+++ conversion_api/mensajeria/task.py
@@ -28,15 +28,15 @@ def process_files(task):
     if format_to_convert == 'tarbz2' or format_to_convert == 'tar.bz2' or format_to_convert == 'bz2':
         convert_to_bz2(origin_file)
         print('hola')
-        x = requests.get('http://localhost:5000/api/process/'+task['id'])
+        x = requests.get('http://127.0.0.1:5000/api/process/'+task['id'])
         print(x.status_code)
     elif format_to_convert == 'zip':
         convert_to_zip(origin_file)
-        x = requests.get('http://localhost:5000/api/process/'+task['id'])
+        x = requests.get('http://127.0.0.1:5000/api/process/'+task['id'])
         print(x.status_code)
     elif format_to_convert == 'tar.gz' or format_to_convert == 'gz' or format_to_convert == 'targz':
         convert_to_gz(origin_file)
-        x = requests.get('http://localhost:5000/api/process/'+task['id'])
+        x = requests.get('http://127.0.0.1:5000/api/process/'+task['id'])
         print(x.status_code)
     else:
         print('not supported format?')
