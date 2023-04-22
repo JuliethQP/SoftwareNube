a88746d4aafd696fb70b72fd97abb5a19c7d035e
SURAMERICANA\juliqupi <jquinchia@sura.com.co>
2023-04-22 12:57:22 -0500

    update ruta archivo

--- conversion_api/apirest/__init__.py
+++ conversion_api/apirest/__init__.py
@@ -8,7 +8,7 @@ import sqlalchemy
 from google.oauth2 import service_account
 from google.cloud.sql.connector import Connector, IPTypes
 
-credential_path = "C:/MAESTRIA/COMPUTACION EN LA NUBE/Proyecto/SoftwareNube/conversion_api/google/proyecto-software-en-la-nube-554f92ee7998.json"
+credential_path = "/home/juliethquinchia/SoftwareNube/conversion_api/google/proyecto-software-en-la-nube-554f92ee7998.json"
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credential_path
 
 def getconn():
