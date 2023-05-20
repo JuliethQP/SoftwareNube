121752ffb2942ce3210917b5012d6a6416120a81
juliqupi <jquinchia@sura.com.co>
2023-05-20 18:48:54 -0500

    Update

--- conversion_api/apirest/__init__.py
+++ conversion_api/apirest/__init__.py
@@ -33,5 +33,4 @@ def create_app(config_name):
         puerto = os.environ.get('URL_MAQUINA_VIRTUAL')  
   
         app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
-        
     return app
