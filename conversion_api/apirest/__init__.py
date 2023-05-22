730fbbac27e54c2af49d4d71f8da58bb11cd98a2
juliqupi <jquinchia@sura.com.co>
2023-05-22 13:14:12 -0500

    Update

--- conversion_api/apirest/__init__.py
+++ conversion_api/apirest/__init__.py
@@ -4,7 +4,7 @@ import os
 from google.cloud.sql.connector import Connector, IPTypes
 
 
-credential_path = "/.../conversion_api/credenciales/proyecto-software-en-la-nube-906bd5b19e9e.json"
+credential_path = "/home/juliethquinchia/SoftwareNube/conversion_api/credenciales/proyecto-software-en-la-nube-906bd5b19e9e.json"
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credential_path
 
 def getconn():
